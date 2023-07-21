a513b07cacd5238178f1d009c6fa93eaced62d8c
UnCompa <125362398+UnCompa@users.noreply.github.com>
2023-07-21 12:31:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,74 @@ navToggle.addEventListener("click",()=>{
         navToggle.setAttribute("aria-label", "Abrir menú");
       }
 })
+const navBoton = document.querySelectorAll(".nav-link")
 
-const boton1 = document.querySelector(".btn1")
-boton1.addEventListener("click",()=>{
-    window.open("https://google.com")
+console.log(navBoton)
+
+navBoton.forEach((boton)=>{
+  boton.addEventListener("click",()=>{
+    navMenu.classList.remove("nav__menu--visible")
+  })
+})
+
+// navBoton.addEventListener("click",()=>{
+//   navMenu.classList.remove("nav__menu--visible")
+// })
+// const boton1 = document.querySelector(".btn1")
+// boton1.addEventListener("click",()=>{
+//     window.open("https://google.com")
+// })
+
+const formulario = document.getElementById("formulario")
+const inputs = document.querySelectorAll("#formulario input")
+
+const expresiones = {
+  nombre: /^[a-zA-Z0-9\_\-\ ]{4,16}$/,
+  correo: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
+  asunto:/^[a-zA-Z0-9]{4,30}$/,
+  area:/^[a-zA-Z0-9]{10,100}$/
+}
+const campos = {
+  nombre: false,
+  correo: false,
+  asunto: false,
+  area: false
+}
+
+const validarFormulario = (e)=>{
+  switch (e.target.name) {
+    case "name":
+      comprobarCampo(expresiones.nombre, e.target, "Nombre")
+    break;
+    case "email":
+      comprobarCampo(expresiones.correo, e.target, "Email")
+    break;
+    case "subject":
+      comprobarCampo(expresiones.asunto, e.target, "Asunto")
+    break;
+    case "comments":
+      comprobarCampo(expresiones.area, e.target, "Area")
+    break;
+  }
+}
+const comprobarCampo = (expresion,input,campo)=>{
+  if (expresion.test(input.value)){
+    document.getElementById(`${campo}`).classList.remove("formulario__input--invalido")
+    document.getElementById(`${campo}`).classList.add("formulario__input--valido")
+    campos[campo] = true
+  } else {
+    document.getElementById(`${campo}`).classList.add("formulario__input--invalido")
+    document.getElementById(`${campo}`).classList.remove("formulario__input--valido")
+    campos[campo] = false
+  }
+}
+inputs.forEach((input)=>{
+  input.addEventListener("keyup", validarFormulario)
+  input.addEventListener("blur", validarFormulario)
+})
+
+formulario.addEventListener('submit', (e)=>{
+  e.defaultPrevented()
+  if(campos.nombre && campos.correo && campos.asunto && campos.area)
+    formulario.reset();
 })
